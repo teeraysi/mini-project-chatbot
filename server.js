850d6381cb9dd6fca021e3ca9489a27f038a35ff
teerayut sinlan <Teerayut2545@hotmail.com>
2019-10-16 23:23:52 +0700

    add some chat

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/healthcheck', (req, res) => res.sendStatus(200));
+const channel_access_token = 'ADjhL97lHtwrhweoFXwe66FozB8Nda9A+4PXDzOsLEOiz8MWZbiMpYAWrMiZ+u9XYshId3diBUb9+aOCPKqKmFOL8lKuf3LnylKS+deWftNXwm1GxUoVOkKEsCZdvXx2vBn6RVbGJpsy43QbtWYi/AdB04t89/1O/w1cDnyilFU=';
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+// app.get('/healthcheck', (req, res) => res.sendStatus(200));
+
+// Line Webhook 
 app.post('/webhook', (req, res) => {
-  res.sendStatus(200)
+
+  let reply_token = req.body.events[0].replyToken;
+  reply(reply_token);
+
+  res.sendStatus(200);
 });
 
 app.listen(port, function () {
   console.log(`${'\u2705'}  Server starting http://localhost:${port}`);
 });
+
+function reply(reply_token) {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + channel_access_token
+  };
+
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: 'text',
+        text: 'Hello'
+      }, {
+        type: 'text',
+        text: 'How are you?'
+      }
+    ]
+  });
+
+  request.post({
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers,
+    body
+  }, (err, res, body) => {
+    console.log('status = ' + res.statusCode);
+  });
+
+}
