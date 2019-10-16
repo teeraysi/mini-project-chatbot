4b1b2682f783132a31f0e32fcf0277de8d6f1b72
teerayut sinlan <Teerayut2545@hotmail.com>
2019-10-16 22:13:20 +0700

    add server

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.get('/healthcheck', (req, res) => res.sendStatus(200));
 app.post('/webhook', (req, res) => {
   res.sendStatus(200)
 });
 
-app.listen(port);
+app.listen(port, function () {
+  console.log(`${'\u2705'}  Server starting http://localhost:${port}`);
+});
